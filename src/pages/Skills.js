0824511fc51ca8d7440f7ccf4c65a4f268ce70b5
David Lee <s.h.david.lee21@gmail.com>
2024-07-31 08:05:09 -0400

    drive

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -150,7 +150,7 @@ function Skills() {
             <h6>
               Get my{" "}
               <a
-                href="https://drive.google.com/file/d/1LaGOILcuhfnnwZHFbclR1kj1xAsvWNdk/view?usp=sharing"
+                href="https://drive.google.com/file/d/1To8ZJ114d1NLra-CfTCCX1TR1VCvdDmk/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
