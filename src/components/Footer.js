0c9aa2ab0607b55197d41b8a173467adda5415dc
David Lee <s.h.david.lee21@gmail.com>
2024-09-24 21:13:07 -0400

    nr

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
     },
     {
       title: "CV",
-      link: "https://drive.google.com/file/d/1dri14R2ttelzeldSmg228FqjYOPj7glq/view?usp=sharing",
+      link: "https://drive.google.com/file/d/1JH1FVSYnaWHlHycsItQwHJZnCXd6UwPQ/view?usp=sharing",
       badge: <GrDocumentPdf size="3rem" />,
     },
   ];
